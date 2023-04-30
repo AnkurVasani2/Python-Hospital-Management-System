0a974b45048323db1cca3b472ad60619bfa4c57b
Khushiisanghavi <119092236+Khushiisanghavi@users.noreply.github.com>
2023-05-01 02:25:39 +0530

    Update billing.py

--- billing.py
+++ billing.py
@@ -9,50 +9,57 @@ mydb = mysql.connector.connect(
 )
 
 # Create the GUI window
+
 root = tk.Tk()
 root.title("Hospital Bill Invoice")
 root.config(bg='#FEFFB7')
+root.geometry("800x600+100+100")
 
 # Create labels and entry widgets for user input
-label0 = tk.Label(root, text="Patient Name:", bg='#FEFFB7')
-label0.grid(row=0, column=0)
+label0 = tk.Label(root, text="Patient Name:", font=('Times New Roman', 12), bg='#FEFFB7')
+label0.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 entry0 = tk.Entry(root)
 entry0.grid(row=0, column=1)
 
-label1 = tk.Label(root, text="Patient ID:", bg='#FEFFB7')
-label1.grid(row=1, column=0)
+label1 = tk.Label(root, text="Patient ID:", font=('Times New Roman', 12), bg='#FEFFB7')
+label1.grid(row=0, column=2, padx=5, pady=5, sticky="w")
 entry1 = tk.Entry(root)
-entry1.grid(row=1, column=1)
+entry1.grid(row=0, column=3)
 
-label2 = tk.Label(root, text="Patient Email:", bg='#FEFFB7')
-label2.grid(row=2, column=0)
+label2 = tk.Label(root, text="Patient Email:", font=('Times New Roman', 12), bg='#FEFFB7')
+label2.grid(row=0, column=4, padx=5, pady=5, sticky="w")
 entry2 = tk.Entry(root)
-entry2.grid(row=2, column=1)
+entry2.grid(row=0, column=5)
 
-label00 = tk.Label(root, text="Contact Number:", bg='#FEFFB7')
-label00.grid(row=3, column=0)
-entry00 = tk.Entry(root)
-entry00.grid(row=3, column=1)
-
-label3 = tk.Label(root, text="Insurance ID:", bg='#FEFFB7')
-label3.grid(row=4, column=0)
+label3 = tk.Label(root, text="Contact Number:", font=('Times New Roman', 12), bg='#FEFFB7')
+label3.grid(row=1, column=0, padx=5, pady=5, sticky="w")
 entry3 = tk.Entry(root)
-entry3.grid(row=4, column=1)
+entry3.grid(row=1, column=1)
 
-label4 = tk.Label(root, text="Room Charge:", bg='#FEFFB7')
-label4.grid(row=5, column=0)
+label4 = tk.Label(root, text="Insurance ID:", font=('Times New Roman', 12), bg='#FEFFB7')
+label4.grid(row=1, column=2, padx=5, pady=5, sticky="w")
 entry4 = tk.Entry(root)
-entry4.grid(row=5, column=1)
+entry4.grid(row=1, column=3)
 
-label5 = tk.Label(root, text="Doctor's Fee:", bg='#FEFFB7')
-label5.grid(row=6, column=0)
+label5 = tk.Label(root, text="Patient Age:", font=('Times New Roman', 12), bg='#FEFFB7')
+label5.grid(row=1, column=4, padx=5, pady=5, sticky="w")
 entry5 = tk.Entry(root)
-entry5.grid(row=6, column=1)
+entry5.grid(row=1, column=5)
 
-label6 = tk.Label(root, text="Medication Charges:", bg='#FEFFB7')
-label6.grid(row=7, column=0)
+label6 = tk.Label(root, text="Room Charge:", font=('Times New Roman', 12), bg='#FEFFB7')
+label6.grid(row=2, column=0, padx=5, pady=5, sticky="w")
 entry6 = tk.Entry(root)
-entry6.grid(row=7, column=1)
+entry6.grid(row=2, column=1)
+
+label7 = tk.Label(root, text="Doctor's Fee:", font=('Times New Roman', 12), bg='#FEFFB7')
+label7.grid(row=2, column=2, padx=5, pady=5, sticky="w")
+entry7 = tk.Entry(root)
+entry7.grid(row=2, column=3)
+
+label8 = tk.Label(root, text="Medication Charges:", font=('Times New Roman', 12), bg='#FEFFB7')
+label8.grid(row=2, column=4, padx=5, pady=5, sticky="w")
+entry8 = tk.Entry(root)
+entry8.grid(row=2, column=5)
 
 # Create a button to calculate the total bill
 def calculate_bill():
@@ -60,13 +67,13 @@ def calculate_bill():
     patient_name = entry0.get()
     patient_id = entry1.get()
     patient_email = entry2.get()
-    contact_number = entry00.get()
-    insurance_id = entry3.get()
-    room_charge = float(entry4.get())
-    lab_fees = float(entry5.get())
-    med_charges = float(entry6.get())
+    contact_number = entry3.get()
+    insurance_id = entry4.get()
+    patient_age = entry5.get()
+    room_charge = float(entry6.get())
+    doctor_fee = float(entry7.get())
+    medication_charges = float(entry8.get())
 
-    # Calculate total bill
     total = room_charge + lab_fees + med_charges
 
     # Display total bill
@@ -75,9 +82,11 @@ def calculate_bill():
 button = tk.Button(root, text="Calculate", command=calculate_bill, bg='#E6E6FA')
 button.grid(row=10, column=1)
 
+
 # Create a label to display the total bill
-label4 = tk.Label(root, text="Total Bill:", bg='#FEFFB7')
-label4.grid(row=11, column=0)
+label9 = tk.Label(root, text="Total Bill:", bg='#FEFFB7')
+label9.grid(row=11, column=0, padx=10)
+
 total_bill = tk.Label(root, text="", bg='#FEFFB7')
 total_bill.grid(row=11, column=1)
 
